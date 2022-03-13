38903a61a5d8466dd1890bc50778ef6bec75bde5
yeljouh <yassine.eljouhari@nestle.com>
2022-03-13 21:34:38 +0100

    init

---
+++ src/com/sqli/training/training.java
@@ -0,0 +1,10 @@
+package com.sqli.training;
+
+public class training {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Aucun souci :)");
+    }
+
+}
