a80e2313069f548d4f2a1da5ce5d2813af3b1f46
OmaimaEl-youssefy <omaimaelyo@gmail.com>
2022-03-14 15:40:16 +0100

    second commit

--- src/com/sqli/training/training.java
+++ src/com/sqli/training/training.java
@@ -5,7 +5,6 @@ public class training {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("Aucun souci :)");
-        System.out.println("omaima");
     }
 
 }
