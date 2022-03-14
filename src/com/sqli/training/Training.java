9ebd5c8994f93c060dfc298295ea292a7d0dfc53
Anir Elmoussaid <anir.elmoussaid@gmail.com>
2022-03-14 15:45:31 +0100

    test-training-hello-world

---
+++ src/com/sqli/training/Training.java
@@ -0,0 +1,10 @@
+package com.sqli.training;
+
+public class Training {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello world ----- test ----- test ----- test ----- test----- test ----- test");
+    }
+
+}
