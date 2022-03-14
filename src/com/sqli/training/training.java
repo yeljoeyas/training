ca52786b0eac34ad36fff9d9df8579b696b8b31a
hafid-laasri <hafid.laasri1234@gmail.com>
2022-03-14 15:42:11 +0100

    commit de test

--- src/com/sqli/training/training.java
+++ src/com/sqli/training/training.java
@@ -5,6 +5,12 @@ public class training {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("Aucun souci :)");
+        System.out.printf(sayHello());
+    }
+
+
+    public static String sayHello(){
+        return "hello every one";
     }
 
 }
